4cea1e1a50d4c212a8fd762472c893f9b3c00a97
peij <peij@htdatacloud.com>
2017-06-14 09:17:59 +0800

    测试google是否可用

---
+++ Tongs/scripts/status.js
@@ -0,0 +1,20 @@
+function httpRequest(url, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(true);
+        }
+    }
+    xhr.onerror = function(){
+        callback(false);
+    }
+    xhr.send();
+}
+
+setInterval(function(){
+    httpRequest('https://www.google.com/', function(status){
+		console.log(status);
+        chrome.browserAction.setIcon({path: 'images/'+(status?'online.png':'offline.png')});
+    });
+},5000);
